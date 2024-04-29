45308eb65006ea33ef77d172e34216397655adae
vet3x <vet3x@outlook.com>
2024-04-29 19:44:37 +0800

    Update .config/fish/config.fish Update .config/fish/fish_plugins Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,3 +5,10 @@ end
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/wsl/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
