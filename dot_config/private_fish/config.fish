a8407fbc8a5e9dd9425ed04eaee5f82b49967458
vet3x <vet3x@outlook.com>
2024-10-01 22:57:04 +0800

    update .config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,8 +1,10 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# if status is-interactive
+# Commands to run in interactive sessions can go here
+# end
 
 # if use brew
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+if command -v brew >/dev/null 2>&1; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
 
 starship init fish | source
