c4f91a7836dd742b2016f47ab5e10d09525fca38
{{ .gitname }} <{{ .gitemail }}>
2026-03-13 14:28:26 +0800

    feat: update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,20 @@
+# 判断命令是否存在的函数
+function has_cmd
+    command -v $argv[1] >/dev/null 2>&1
+end
+
+# -----------------------------
 # Homebrew
-type -q brew; and brew shellenv | source
+# -----------------------------
+if test -d /home/linuxbrew/.linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+else if test -d /opt/homebrew
+    eval (/opt/homebrew/bin/brew shellenv)
+else if test -d /usr/local/Homebrew
+    eval (/usr/local/bin/brew shellenv)
+end
 
 # Starship prompt
-type -q starship
-and starship init fish | source
+if has_cmd starship
+    starship init fish | source
+end
